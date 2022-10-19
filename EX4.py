7648d59a66a6f4663364e5613bb81341c524afc0
HAKIMA ARKHA <115365535+hakima2022@users.noreply.github.com>
2022-10-19 06:06:26 +0200

    Update EX4.py

--- EX4.py
+++ EX4.py
@@ -1,6 +1,10 @@
+
+
 from PIL import Image
 img=Image.open("image.jpg")
 from PIL import ImageDraw
-
-
+I1=ImageDraw.Draw(img)
+from PIL import ImageFont
+font=ImageFont.truetype('calibri',25)
+I1.text((331,142),"L1=104,7mm",fill=(255,255,255,255),font=font)
 
